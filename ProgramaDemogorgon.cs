578e041de9f3b2aeb4bdd6fb2b074e23bae1bc36
TomasBedoya09 <tomasalejandrobedoyalopez5@gmail.com>
2024-05-16 15:32:33 -0500

    Creación programa Stranger Things
    Búsqueda de registro para criaturas del mundo

---
+++ ProgramaDemogorgon.cs
@@ -0,0 +1,16 @@
+/******************************************************************************
+Los personajes de Stranger Things necesitan un programa 
+en C# para registrar los encuentros con criaturas del
+mundo como el Demogorgon. Completa el espacio en blanco
+para imprimir el nombre de la criatura en la consosola
+
+*******************************************************************************/
+using System;
+class Program
+{
+    static void Main (string[] args)
+    {
+        string nombreCriatura = "Demogorgon";
+        Console.WriteLine("nombreCriatura es " + nombreCriatura );
+    }
+}
