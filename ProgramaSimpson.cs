08660a57b0a28ea78a8e073a2618d93fbb4b0033
TomasBedoya09 <tomasalejandrobedoyalopez5@gmail.com>
2024-05-16 16:30:00 -0500

    Programa de competencia de los Simpson
    busqueda de creacion para la competencia de los simpsons

---
+++ ProgramaSimpson.cs
@@ -0,0 +1,24 @@
+/******************************************************************************
+autores:Tomas Bedoya
+Kevin Vera
+fecha:16-05-2024
+descripcion:Los simpson estan organizando un torneo de videojuegos y necesitan un programa en
+c# para mostrar los nombres de lso participantes . completa el espacio en blanco
+para imprimir los nombres  en la consola:
+
+*******************************************************************************/
+using System;
+class programa
+{
+  static void Main() {
+      string participante1 = "Bart";
+      string participante2 = "Lisa";
+      string participante3 = "Milhouse";
+      string participante4 = "Nelson";
+      Console.WriteLine("pon en orden estos participantes: ");
+      Console.WriteLine( participante1 );
+      Console.WriteLine( participante2 );
+      Console.WriteLine( participante3 );
+      Console.WriteLine( participante4 );
+  }
+}
