74911c2b5d25918f8728b66b6f33c449097f0ea7
TomasBedoya09 <tomasalejandrobedoyalopez5@gmail.com>
2024-05-22 16:53:39 -0500

    Programa de descuentos
    Programa para saber el descuento total de la compra

---
+++ ProgramaDescuentos.cs
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Autor: Tomas A. Bedoya
+Fecha: 2024-05-22
+Descripcion: 
+Tony Stark, Iron Man, quiere saber cuanto dinero debe 
+tener para comprar X tornillos c/u a $300, pero en la tienda 
+le dan descuentos segun cantidad comprada.
+Crear un algoritmo que ayude a calcular si compra:
+    1 sin descuento,
+    5 descuento del 30%;
+    10 descuento del 50%,
+*******************************************************************************/
+using System;
+class ProgramaDescuentos{
+    static void Main() {
+        
+    // variables
+    int cantidadTornillos;
+    int cantidadDescuento
+    
+    // peticion de datos
+    Console.WriteLine("Cantidad de tornillos: " + cantidadTornillos);
+    Console.WriteLine("Precio unitario: " + precioUnitario);
+    Console.WriteLine("Total a pagar sin descuento: " + totalSinDescuento);
+    
+    // validacion 
+    if(cantidadTornillos >= 100){
+        Console.WriteLine("")
+    }
+    
+    }
+}
